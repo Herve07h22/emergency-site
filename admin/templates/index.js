bded92889b2efb5200d47be8b6ab7644c7d5efa4
Herve07h22 <herve.rincent@free.fr>
2020-03-24 15:18:41 +0100

    french translation

--- admin/templates/index.js
+++ admin/templates/index.js
@@ -1,4 +1,6 @@
 import Post from '/admin/templates/post.js'
+//import fr from 'https://unpkg.com/netlify-cms-locales@1.10.0/dist/esm/fr/index.js?module'
 
 CMS.registerPreviewTemplate('posts', Post)
 CMS.registerPreviewStyle('/assets/css/style.css')
+//CMS.registerLocale('fr', fr);
--- src/posts/2020-03-22-example-post.md
+++ src/posts/2020-03-22-example-post.md
@@ -1,5 +1,5 @@
 ---
-title: Crisis Information Example Post
+title: Exemple d'article d'information
 date: 2020-03-22T14:00
 ---
 
